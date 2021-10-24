9ab37265438ffcb3a1f69b630fd9487b625e2486
Emeriscus <kimre88@gmail.com>
2021-10-24 19:27:37 +0200

    Day02 final v1.0

---
+++ week02-labs-tasks/src/main/java/Day02/Geometry.java
@@ -0,0 +1,9 @@
+package Day02;
+
+public class Geometry {
+    public static void main(String[] args) {
+
+        Rectangle rectangle = new Rectangle(3, 4.5);
+        System.out.println(rectangle.calculateArea());
+    }
+}
